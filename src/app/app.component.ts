276a0adacb95fc3e5468259affc76c447594acc6
aotaduy <aotaduy@users.noreply.github.com>
2020-05-11 14:15:22 -0300

    component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ttads';
+ list = [];
+
+ add(inputControl) {
+   if (inputControl.value === '') {
+     return;
+   }
+   this.list.push(
+     {
+       description: inputControl.value,
+      completed: false
+    });
+   inputControl.value = '';
+ }
+ remove(item) {
+   const index = this.list.findIndex(each => each.description === item.description);
+   this.list.splice(index, 1);
+ }
+ toggle(item) {
+   item.completed = !item.completed
+ }
 }
+
