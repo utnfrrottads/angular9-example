4f880810579c463b8e70d6c3d9624564e1cb6d68
Oscar Dorronzoro <oscar.dorronzoro@gmail.com>
2020-05-23 19:25:33 -0300

    agregado de atributo url de imagen

--- src/app/model/todo-item.ts
+++ src/app/model/todo-item.ts
@@ -1,16 +1,39 @@
 export class TodoItem {
   id: number;
+  imageURL: string;
   description: string;
   isCompleted: boolean;
   dateTimeCreation: Date;
   dateTimeCompleted: Date;
 
+  static createTodoItem(item){
+    let newItem= new TodoItem();
+    newItem.id=item.id;
+    newItem.description=item.description;
+    newItem.isCompleted=item.isCompleted;
+    newItem.dateTimeCreation=item.dateTimeCreation;
+    newItem.dateTimeCompleted=item.dateTimeCompleted;
+    newItem.imageURL=item.imageURL;
+    return newItem;
+  }
+
   constructor(){
     this.isCompleted=false;
     this.dateTimeCreation=new Date();
+    this.imageURL="";
   }
 
   toggleCompleted() {
     this.isCompleted = !this.isCompleted;
+    if(this.isCompleted){
+      this.dateTimeCompleted=new Date();
+    }
+    else{
+      this.dateTimeCompleted=undefined;
+    }
+  }
+
+  getCompletedTime(){
+    return this.dateTimeCompleted.getTime() - this.dateTimeCreation.getTime();
   }
 }
