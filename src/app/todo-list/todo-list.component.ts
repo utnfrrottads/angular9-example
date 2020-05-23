a25bae63ffb29d1c2bbde4b6cdb0e6edfb6e168b
Oscar Dorronzoro <oscar.dorronzoro@gmail.com>
2020-05-23 19:23:59 -0300

    modificacion de tarea

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -8,10 +8,15 @@ import { TodoItem } from '../model/todo-item';
 })
 export class TodoListComponent{
 
-  @Input() list: TodoItem[]=[];
+  @Input() list: TodoItem[] = [];
   @Output() itemRemoved = new EventEmitter();
   @Output() itemStateChanged = new EventEmitter();
-
+  @Output() itemUpdated = new EventEmitter();
+  editionModeEnabledTo: Number;
+  //imagesURL="/root/Angular/angular9-example/node_modules/bootstrap-icons/icons/"
+  imagesURL="../../../images/";
+  imagesList=["cart.png","heart.png","light_bulb.png","like.png","text.png"];
+  selectedImage='';
 
   toggleCompleted(item: TodoItem){
     this.itemStateChanged.emit(item);
@@ -20,4 +25,23 @@ export class TodoListComponent{
   deleteTodoItem(item: TodoItem){
     this.itemRemoved.emit(item);
   }
+
+  updateTodoItem(item: TodoItem){
+    this.toggleEdition(item);
+    this.itemUpdated.emit(item);
+  }
+
+  toggleEdition(item: TodoItem){
+    if(item.id===this.editionModeEnabledTo){
+      this.editionModeEnabledTo=undefined;
+    }
+    else{
+      this.editionModeEnabledTo=item.id;
+    }
+  }
+
+  onImageChanged(imageChosen){
+    this.selectedImage=imageChosen.options[imageChosen.selectedIndex].value
+  }
+
 }
