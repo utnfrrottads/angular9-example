354839052ab5a741014a4b73b733a3337d67f58e
Danilo Bassi <danilo-bassi@hotmail.com>
2020-06-10 20:52:15 -0300

    agregado de LocalStorage para persistir la lista

--- src/app/todo-app/todo-app.component.ts
+++ src/app/todo-app/todo-app.component.ts
@@ -1,18 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import {TodoItem} from '../model/todo-item';
+import { TodoItem } from '../model/todo-item';
 import { element } from 'protractor';
 import { TodoService } from '../todo.service';
+import { LocalStorageService } from '../local-storage.service';
 /** */
 @Component({
   selector: 'app-todo',
   templateUrl: './todo-app.component.html',
   styleUrls: ['./todo-app.component.scss'],
 })
-export class TodoAppComponent  {
-
+export class TodoAppComponent implements OnInit {
   constructor(
-    private service: TodoService
+    private service: TodoService,
+    private storage: LocalStorageService
   ) {}
+
+  ngOnInit(): void {
+    
+    let oldList = this.storage.getList();
+    if (oldList === null) {
+      console.log("NO hay lista previa.");
+    }
+    else{
+      this.service.updateList(oldList);
+    }
+  }
 
   getList() {
     return this.service.list;
@@ -21,9 +33,9 @@ export class TodoAppComponent  {
     this.service.remove(id);
   }
   onItemStateChanged(item: TodoItem) {
-    item.toggleCompleted();
+    this.service.toggleCompleted(item);
   }
   onTodoItemCreated(task) {
-    this.service.add(task)
+    this.service.add(task);
   }
 }
