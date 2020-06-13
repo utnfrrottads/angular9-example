c8fc5dd4bfb14ea02c457fc559ef4770ef3d8abe
Alejandro Recalde <alejandrorecalde5@gmail.com>
2020-06-13 19:03:40 -0300

    Implement local-storage service and make it a dependency of todo-service

--- src/app/local-storage.service.ts
+++ src/app/local-storage.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { TodoItem } from './model/todo-item';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LocalStorageService {
   // https://developer.mozilla.org/es/docs/Web/API/Window/localStorage
-  constructor() { }
+  constructor() {}
+
+  updateStorage(todoItems: TodoItem[]): void {
+    localStorage.setItem('todo-list', JSON.stringify(todoItems));
+  }
+
+  getItemsInStorage(): TodoItem[] | null {
+    return JSON.parse(localStorage.getItem('todo-list'));
+  }
 }
--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -2,32 +2,36 @@ import { Injectable } from '@angular/core';
 import { LocalStorageService } from './local-storage.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoService {
-  
   list = [];
   lastItemId = 0;
 
-  constructor(private storage: LocalStorageService) { }
-  
+  constructor(private storage: LocalStorageService) {
+    this.list = storage.getItemsInStorage() == null? []: storage.getItemsInStorage();
+  }
+
   add(task) {
     const id = this.lastItemId;
     task.id = id;
     this.list.push(task);
     this.lastItemId += 10;
+
+    this.storage.updateStorage(this.list);
   }
 
   remove(id) {
     const index = this.list.findIndex((element) => element.id === id);
     this.list.splice(index, 1);
+
+    this.storage.updateStorage(this.list);
   }
 
   uncompletedTaskNumber() {
-    return this.list.filter(item => !item.isCompleted).length;
-
+    return this.list.filter((item) => !item.isCompleted).length;
   }
   completedTaskNumber() {
-    return  this.list.filter(item => item.isCompleted).length ;
+    return this.list.filter((item) => item.isCompleted).length;
   }
 }
