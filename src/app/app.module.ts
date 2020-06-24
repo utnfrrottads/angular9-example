eae7d406de9a33212798e35bd9f51c01a1a8a322
aotaduy <aotaduy@users.noreply.github.com>
2020-06-24 19:53:16 -0300

    http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticlesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/articles.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArticlesService {
+
+  readonly baseUrl = 'https://conduit.productionready.io/api/';
+
+  constructor(private http: HttpClient) { }
+  getArticles() {
+    const url = this.baseUrl + 'articles';
+    return this.http.get<any>(url);
+  }
+  getTags() {
+    const url = this.baseUrl + 'tags';
+    return this.http.get<any>(url);
+  }
+}
