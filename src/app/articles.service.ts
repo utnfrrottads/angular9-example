cce6b2cb1f46f5f0864b13619e7e86d6f43e8313
Alejandro Recalde <alejandrorecalde5@gmail.com>
2020-06-27 19:48:26 -0300

    Update article service: query string parameters and type safety supported

--- src/app/articles.service.ts
+++ src/app/articles.service.ts
@@ -1,20 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ArticleList } from './model/article-list';
+import { TagList } from './model/tag-list';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ArticlesService {
+  readonly baseUrl = 'https://conduit.productionready.io/api';
 
-  readonly baseUrl = 'https://conduit.productionready.io/api/';
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-  getArticles() {
-    const url = this.baseUrl + 'articles';
-    return this.http.get<any>(url);
+  getArticles(params: { tag: string; limit: number }): Observable<ArticleList> {
+    const url = `${this.baseUrl}/articles?tag=${params.tag}&limit=${params.limit}`;
+    return this.http.get<ArticleList>(url);
   }
-  getTags() {
-    const url = this.baseUrl + 'tags';
-    return this.http.get<any>(url);
+  getTags(): Observable<TagList> {
+    const url = `${this.baseUrl}/tags`;
+    return this.http.get<TagList>(url);
   }
 }
