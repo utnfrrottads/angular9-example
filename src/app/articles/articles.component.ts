e672cd44265fd66e4455aa704d9ea9c437557638
Alejandro Recalde <alejandrorecalde5@gmail.com>
2020-06-27 22:03:47 -0300

    Implement comment functionality on articles and feed components

--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ArticlesService } from '../articles.service';
 import { Article } from '../model/article';
+import { UserData } from '../model/user-data';
 
 @Component({
   selector: 'app-articles',
@@ -9,8 +10,18 @@ import { Article } from '../model/article';
 })
 export class ArticlesComponent implements OnInit {
   @Input() articles: Article[] = [];
-
-  constructor() {}
+  @Input() user: UserData;
+  constructor(private service: ArticlesService) {}
 
   ngOnInit(): void {}
+
+  onCommentSubmit(commentInput, articleSlug) {
+    this.service
+      .commentOnArticle({
+        smug: articleSlug,
+        commentBody: commentInput.value,
+        token: this.user.token,
+      })
+      .subscribe();
+  }
 }
--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Article } from '../model/article';
 import { ArticlesService } from '../articles.service';
+import { AuthService } from '../auth.service';
+import { UserData } from '../model/user-data';
 
 @Component({
   selector: 'app-feed',
@@ -10,23 +12,40 @@ import { ArticlesService } from '../articles.service';
 export class FeedComponent implements OnInit {
   tags: string[] = [];
   articles: Article[] = [];
-  constructor(private service: ArticlesService) {}
+  user: UserData;
+
+  constructor(
+    private articleService: ArticlesService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.loadTags();
+    this.loadUser('somisa@somisa.com', 'somisa2000');
   }
 
   onTagSelected(selectedTag) {
     this.loadArticles(selectedTag, 5);
   }
 
+  loadUser(email: string, password: string) {
+    this.authService
+      .login({
+        email: email,
+        password: password,
+      })
+      .subscribe((response) => (this.user = response.user));
+  }
+
   loadArticles(tag, limit) {
-    this.service
+    this.articleService
       .getArticles({ tag: tag, limit: limit })
       .subscribe((response) => (this.articles = response.articles));
   }
 
   loadTags() {
-    this.service.getTags().subscribe((response) => (this.tags = response.tags));
+    this.articleService
+      .getTags()
+      .subscribe((response) => (this.tags = response.tags));
   }
 }
