e0b37e916a65f83725a27d58fb8e0e659e6ba018
campi97 <53315236+campi97@users.noreply.github.com>
2020-06-30 20:09:59 -0300

    Ejercicio de http

--- src/app/articles.service.ts
+++ src/app/articles.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -7,8 +7,16 @@ import { HttpClient } from '@angular/common/http';
 export class ArticlesService {
 
   readonly baseUrl = 'https://conduit.productionready.io/api/';
-
+  
+  
   constructor(private http: HttpClient) { }
+  usrReturned:any;
+  respuesta:any;
+
+  getTagArticles(tag) {
+    const url = this.baseUrl + 'articles?tag='+tag;
+    return this.http.get<any>(url);
+  }
   getArticles() {
     const url = this.baseUrl + 'articles';
     return this.http.get<any>(url);
@@ -17,4 +25,19 @@ export class ArticlesService {
     const url = this.baseUrl + 'tags';
     return this.http.get<any>(url);
   }
+  setComment(token,comment,slug){
+    const url = this.baseUrl + 'articles/'+ slug +'/comments';
+    const httpOptions = {headers: new HttpHeaders({
+      'Authorization': 'Token '+ token
+    })}
+
+     this.http.post(url, comment, httpOptions).subscribe(response=>{this.respuesta = response});
+     return this.respuesta;
+  }
+  authentication(usuario){
+    const url = this.baseUrl + 'users/login';
+    this.http.post(url,usuario).subscribe(response=>{this.usrReturned = response});
+    return this.usrReturned.user;
+    }
+    
 }
--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ArticlesService } from '../articles.service';
+import { conditionallyCreateMapObjectLiteral } from '@angular/compiler/src/render3/view/util';
 
 @Component({
   selector: 'app-articles',
@@ -10,15 +11,50 @@ export class ArticlesComponent implements OnInit {
 
   articles:any = []
   tags: any = [];
+  user:any;
   constructor(private service: ArticlesService) { }
 
   ngOnInit(): void {
 
   }
-  loadArticles() {
-    this.service.getArticles().subscribe(response => this.articles = response.articles);
-  }
   loadTags() {
-    this.service.getTags().subscribe(response => this.tags = response.tags);
+    this.service.getTags().subscribe(response => this.tags = response.tags);   
+  }
+
+  loadArticles(tagName){
+    this.service.getTagArticles(tagName).subscribe(response => {
+      for(var i=0; i < 5; i++){
+        this.articles[i] = response.articles[i]
+      }
+    });
+
+  }
+  hideArticles(tagName){
+    this.articles = []
+  }
+
+  addComment(article){
+    var userReturned = this.autenticarse();
+
+    console.log(userReturned);
+    //{"user":{"id":103806,"email":"igc@ttadsmail.com","createdAt":"2020-06-29T19:48:39.963Z","updatedAt":"2020-06-29T19:48:39.970Z","username":"igc","bio":null,"image":null,"token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTAzODA2LCJ1c2VybmFtZSI6ImlnYyIsImV4cCI6MTU5ODY0NjY3OX0.ppArFTkskBSIwHbzG1qe-NyLdP3QcjWIAWgjPAm7RrI"}}
+
+    var comentario = {"comment":{
+      "body": (document.getElementById('comentario') as HTMLInputElement).value
+    }}
+  console.log(this.service.setComment(userReturned.token,comentario,article.slug))
+  }
+
+  //user: igc@ttadsmail.com
+  //pass: igcttads2020
+  autenticarse(){
+    var usuario = {
+      "user":{
+        "email": "igc@ttadsmail.com",
+        "password": "igcttads2020"
+      }
+    }
+
+    return this.service.authentication(usuario);
   }
 }
