3e311c715eea798ae3c27d82d17941919d00cc55
Sebastian Moreyra <SebastianJMoreyra@hotmail.com>
2020-08-21 11:18:08 -0300

    pagination-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { HttpClientModule} from '@angular/common/http';
+import { ArticlesComponent } from './components/articles/articles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticlesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgxPaginationModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/articles/articles.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ArticlesService } from 'src/app/services/articles.service';
+
+@Component({
+  selector: 'app-articles',
+  templateUrl: './articles.component.html',
+  styleUrls: ['./articles.component.scss']
+})
+export class ArticlesComponent implements OnInit {
+
+  articles:any = [];
+  page:number = 1;
+  totalItems: string;
+  constructor(private service:ArticlesService) { 
+    
+  }
+
+  ngOnInit(): void {
+    this.articles = this.service.getArticles().subscribe((response) => {
+      this.articles = response.articles;
+    })
+  }
+
+}
