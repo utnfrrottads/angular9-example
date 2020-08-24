ae62c7990749d7c6a8293aed7c5a056149465dc6
Oscar Dorronzoro <oscar.dorronzoro@gmail.com>
2020-08-23 23:28:12 -0300

    app structure initialized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { ArticlesListComponent } from './articles-list/articles-list.component';
+import { CommentsListComponent } from './comments-list/comments-list.component';
+import { CommentFormComponent } from './comment-form/comment-form.component';
+import { ArticleFormComponent } from './article-form/article-form.component';
+import { ArticlePageComponent } from './article-page/article-page.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    SignupComponent,
+    SigninComponent,
+    ErrorPageComponent,
+    ArticlesListComponent,
+    CommentsListComponent,
+    CommentFormComponent,
+    ArticleFormComponent,
+    ArticlePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
