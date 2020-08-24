5d3729144633eb181ebf9bd9c88f1eb9357f6415
Alejandro Recalde <alejandrorecalde5@gmail.com>
2020-08-24 13:34:27 -0300

    Use reactive-forms for the task form

--- src/app/todo-form/todo-form.component.ts
+++ src/app/todo-form/todo-form.component.ts
@@ -1,24 +1,27 @@
-import { Component, OnInit, Output, EventEmitter} from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { TodoItem } from '../model/todo-item';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-form',
   templateUrl: './todo-form.component.html',
-  styleUrls: ['./todo-form.component.scss']
+  styleUrls: ['./todo-form.component.scss'],
 })
 export class TodoFormComponent {
-
+  taskForm = new FormGroup({
+    description: new FormControl('', [Validators.required])
+  });
   @Output() add = new EventEmitter();
 
-  save(description){
-    if(!description.value || description.value === '') {
-      return;
-    }
+  get description() {
+    return this.taskForm.get('description');
+  }
+
+  onSubmit() {
     let task = new TodoItem();
-    task.description = description.value;
+    task.description = this.description.value;
     task.isCompleted = false;
     this.add.emit(task);
-    description.value = '';
+    this.taskForm.reset();
   }
 }
-
