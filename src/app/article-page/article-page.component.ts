36910d932db90c5febecabb596a67bc27bd3f3c5
Oscar Dorronzoro <oscar.dorronzoro@gmail.com>
2020-08-26 17:59:03 -0300

    article details

--- src/app/article-page/article-page.component.ts
+++ src/app/article-page/article-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ArticleService } from '../services/article.service';
+import { Article } from '../model/article';
 
 @Component({
   selector: 'app-article-page',
@@ -7,11 +9,12 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ArticlePageComponent implements OnInit {
 
-  @Input() article;
+  article: Article;
 
-  constructor() { }
+  constructor(private articleService: ArticleService) { }
 
   ngOnInit(): void {
+    this.article = this.articleService.getArticle();
   }
 
 }
--- src/app/comments-list/comments-list.component.ts
+++ src/app/comments-list/comments-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { HttpService } from '../services/http.service';
+import { Comment } from '../model/comment';
 
 @Component({
   selector: 'app-comments-list',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommentsListComponent implements OnInit {
 
-  constructor() { }
+  comments: Comment[] = [];
+  @Input() article;
+
+  constructor(private http: HttpService) { }
 
   ngOnInit(): void {
+    this.http.getAllCommentsByArticle(this.article).subscribe(response => this.comments = response.comments);
   }
 
 }
