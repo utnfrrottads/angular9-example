d252a858220181a3224dbb43efae1ffd6fd4ac75
Oscar Dorronzoro <oscar.dorronzoro@gmail.com>
2020-08-27 19:41:49 -0300

    home page styles improved, paginator

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -10,19 +10,35 @@ import { Article } from '../model/article';
 export class HomePageComponent implements OnInit {
 
   articles: Article[] = [];
+  tags: string[] = [];
+  readonly pageLimit = 10;
+  pages: number[] = [];
 
   constructor(private http: HttpService) { }
 
   ngOnInit(): void {
-    this.getAllArticles();
+    this.getAllArticles(1);
+    this.getAllTags();
   }
 
-  getAllArticles(){
-    this.http.getAllArticles().subscribe(response => this.articles = response.articles );
+  getAllArticles(page: number){
+    this.http.getAllArticles(page, this.pageLimit).subscribe(response => {
+        this.articles = response.articles;
+        const pagesCount = (Math.ceil(response.articlesCount / this.pageLimit));
+        
+        this.pages = [];
+        for(let i=0; i < pagesCount; i++){
+          this.pages.push(i+1);
+        }
+      });
   }
 
-  getMyArticles(){
+  getAllTags(){
+    this.http.getAllTags().subscribe(response => this.tags = response.tags);
+  }
+
+  getMyArticles(page: number){
     this.articles=[]; //test
-    //this.http.getMyArticles().subscribe(response => this.articles = response.articles );
+    //this.http.getMyArticles(page).subscribe(response => this.articles = response.articles );
   }
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
-import { HttpService } from '../http.service';
+import { HttpService } from '../services/http.service';
 
 @Component({
   selector: 'app-signup',
