39d0ae30847b32e12f8d9fcf2d942d71a93c2342
Agustina Blanco <agustinablanco2524@gmail.com>
2020-08-28 16:49:00 -0300

    hide navigation bar item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from './services/local-storage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-peti';
+  token = '';
+
+  constructor(private storage: LocalStorageService){}
+
+  ngOnInit(){
+    this.token = this.storage.getAuthentication();
+  }
 }
