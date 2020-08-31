ef946982c4483a233b6fc6378a2f33bf03d7e96b
Alejo Cuello <alejocuello1998@gmail.com>
2020-08-31 07:08:42 -0300

    Sign up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
-import { ArticleComponent } from './components/article/article.component';
+import { ArticleListComponent } from './components/article-list/article-list.component';
 import { CommentComponent } from './components/comment/comment.component';
 import { UserComponent } from './components/user/user.component';
-import { HttpClientModule } from '@angular/common/http';
+import { ArticleFormComponent } from './components/article-form/article-form.component';
+import { UserSignupComponent } from './components/user-signup/user-signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArticleComponent,
+    ArticleListComponent,
     CommentComponent,
-    UserComponent
+    UserComponent,
+    ArticleFormComponent,
+    UserSignupComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
