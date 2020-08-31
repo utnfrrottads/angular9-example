a781f82e790233042d525210f1f8b368582ee113
Alejo Cuello <alejocuello1998@gmail.com>
2020-08-30 22:38:37 -0300

    Routing design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './components/user/user.component';
+import { UserSignupComponent } from './components/user-signup/user-signup.component';
+import { ArticleListComponent } from './components/article-list/article-list.component';
+import { CommentComponent } from './components/comment/comment.component';
+import { ArticleFormComponent } from './components/article-form/article-form.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'signin', pathMatch: 'full' },
   { path: 'signin', component: UserComponent },
+  { path: 'signup', component: UserSignupComponent},
+  { path: 'articles', component: ArticleListComponent },
+  { path: 'article/:slug', component: CommentComponent },
+  { path: 'editArticle/:slug', component: ArticleFormComponent },
+  { path: 'newArticle', component: ArticleFormComponent},
   { path: '**', redirectTo: 'login' },
 ];
 
