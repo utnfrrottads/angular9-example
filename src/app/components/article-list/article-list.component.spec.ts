ac36ea0cb0ee8fa19db67e25e770fcf28a145a62
Alejo Cuello <alejocuello1998@gmail.com>
2020-08-31 09:49:43 -0300

    Article list and form

--- src/app/components/article-list/article-list.component.spec.ts
+++ src/app/components/article-list/article-list.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ArticleComponent } from './article.component';
+import { ArticleComponent } from './article-list.component';
 
 describe('ArticleComponent', () => {
   let component: ArticleComponent;
--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -1,15 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormControl, Validators } from '@angular/forms';
+
+import { CommentsService } from 'src/app/services/comments.service';
+import { ArticlesService } from 'src/app/services/articles.service';
 
 @Component({
   selector: 'app-comment',
   templateUrl: './comment.component.html',
-  styleUrls: ['./comment.component.scss']
+  styleUrls: ['./comment.component.scss'],
 })
 export class CommentComponent implements OnInit {
+  token: any;
+  article: any;
+  slug: any;
+  comments: any = [];
+  comment: any;
+  commentControl = new FormControl('', [Validators.required]);
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private articleService: ArticlesService,
+    private commentService: CommentsService
+  ) {}
 
   ngOnInit(): void {
+    if (localStorage.getItem('token') === null) {
+      this.router.navigate(['login']);
+    }
+    this.slug = this.route.snapshot.paramMap.get('slug');
+    this.article = this.articleService
+      .getArticleBySlug(this.slug)
+      .subscribe((res) => {
+        this.article = res.article;
+        this.comments = this.getComments(res.article.slug);
+      });
   }
 
+  setComment() {
+    if (this.commentControl.value !== null) {
+      this.token = localStorage.getItem('token');
+      this.comment = {
+        comment: {
+          body: this.commentControl.value,
+        },
+      };
+
+      this.commentService
+        .setComment(this.comment, this.slug, this.token)
+        .subscribe((res) => {
+          this.commentControl.patchValue('');
+          this.getComments(this.slug);
+        });
+    }
+  }
+
+  deleteComment(commentId) {
+    this.token = localStorage.getItem('token');
+    this.commentService
+      .deleteComment(commentId, this.article.slug, this.token)
+      .subscribe((res) => {
+        this.getComments(this.slug);
+      });
+  }
+
+  getComments(article) {
+    this.commentService.getComments(article).subscribe((response) => {
+      this.comments = response.comments;
+    });
+  }
+
+  canEdit(comment) {
+    if (comment.author.username === localStorage.getItem('username')) {
+      return true;
+    }
+    else{
+    return false;
+    }
+  }
 }
