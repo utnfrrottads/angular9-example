d4aad95d3f801dc57f254f3a8a2cd3f9f35a524e
Oscar Dorronzoro <oscar.dorronzoro@gmail.com>
2020-09-06 13:44:01 -0300

    update article

--- src/app/article-form/article-form.component.ts
+++ src/app/article-form/article-form.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { HttpService } from '../services/http.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ArticleService } from '../services/article.service';
+import { Article } from '../model/article';
 
 @Component({
   selector: 'app-article-form',
@@ -15,21 +18,66 @@ export class ArticleFormComponent implements OnInit {
     body: new FormControl(''),
     tagList: new FormControl('')
   });
-  @Input() article;
 
-  constructor(private http: HttpService) { }
+  mode: string;
+  article: Article;
+
+  constructor(
+    private http: HttpService,
+    private route: ActivatedRoute,
+    private articleService: ArticleService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe( params => this.mode = params.mode);
+    
+    switch(this.mode){
+      case 'update':
+        this.articleForm.get('tagList').disable();
+        this.article = this.articleService.getArticle();
+        if(this.article !== undefined){
+          this.articleForm.patchValue(this.article);
+        }
+        break;
+      case 'create':
+        break;
+      default:
+        this.router.navigate(['error']);
+    }
+
   }
 
   saveArticle(){
-    this.createArticle();
+    switch(this.mode){
+      case 'update': this.updateArticle();
+        break;
+      case 'create': this.createArticle();
+        break;
+    }
   }
 
   createArticle(){
     this.http.createArticle(this.articleForm.value).subscribe(response => {
       if(response.errors !== undefined){
-        alert('Error al enviar articulo')
+        alert('Error when creating article');
+      }
+      else{
+        alert('Article successfully created');
+        this.router.navigate(['home/myArticles/1']);
+      }
+    });
+  }
+
+  updateArticle(){
+    Object.assign(this.article, this.articleForm.value);
+    this.http.updateArticle(this.article).subscribe( response => {
+      if(response.errors !== undefined){
+        alert('Error when updating article');
+      }
+      else{
+        alert('Article successfully updated');
+        this.router.navigate(['home/myArticle/1']);
       }
     });
   }
