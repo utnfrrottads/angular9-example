7a02cdfdf5dc3c4ceeb18523f22ef90bb319310c
Agustina Blanco <agustinablanco2524@gmail.com>
2020-09-08 00:23:45 -0300

    create comment

--- src/app/comment-form/comment-form.component.ts
+++ src/app/comment-form/comment-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpService } from '../services/http.service';
 
 @Component({
   selector: 'app-comment-form',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommentFormComponent implements OnInit {
 
-  constructor() { }
+  commentForm = new FormGroup({
+    body: new FormControl('',[Validators.required, Validators.maxLength(500)])
+  })
+
+  @Input() article;
+  @Output() commentSent = new EventEmitter();
+
+  constructor(
+    private http: HttpService
+  ) { }
 
   ngOnInit(): void {
+    console.log(this.article);
   }
 
+  sendComment(){
+    this.http.addCommentToArticle(this.article, this.commentForm.value).subscribe( response => {
+      if(response.errors !== undefined){
+        alert('Error when commenting');
+      }
+      else{
+        this.commentSent.emit(this.article);
+        alert('Successfully commented');
+      }
+    });
+  }
 }
