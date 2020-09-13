f3c85965e81210d40c9ab9c246cec122243ebfdc
Oscar Dorronzoro <oscar.dorronzoro@gmail.com>
2020-09-13 15:30:35 -0300

    comment styles improved

--- src/app/comment-form/comment-form.component.ts
+++ src/app/comment-form/comment-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { HttpService } from '../services/http.service';
+import { LocalStorageService } from '../services/local-storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-comment-form',
@@ -17,22 +19,31 @@ export class CommentFormComponent implements OnInit {
   @Output() commentSent = new EventEmitter();
 
   constructor(
-    private http: HttpService
+    private http: HttpService,
+    private storage: LocalStorageService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
-    console.log(this.article);
+    
   }
 
   sendComment(){
-    this.http.addCommentToArticle(this.article, this.commentForm.value).subscribe( response => {
-      if(response.errors !== undefined){
-        alert('Error when commenting');
-      }
-      else{
-        this.commentSent.emit(this.article);
-        alert('Successfully commented');
-      }
-    });
+    if(!this.storage.getAuthentication()){
+      alert('You need to log in');
+      this.router.navigate(['login']);
+    }
+    else{
+      this.http.addCommentToArticle(this.article, this.commentForm.value).subscribe( response => {
+        if(response.errors !== undefined){
+          alert('Error when commenting');
+        }
+        else{
+          this.commentSent.emit(this.article);
+        }
+      });
+      this.commentForm.reset();
+    }
+    
   }
 }
--- src/app/comments-list/comments-list.component.ts
+++ src/app/comments-list/comments-list.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { HttpService } from '../services/http.service';
 import { Comment } from '../model/comment';
 import { User } from '../model/user';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Author } from '../model/author';
+import { LocalStorageService } from '../services/local-storage.service';
 
 @Component({
   selector: 'app-comments-list',
@@ -11,18 +14,32 @@ import { User } from '../model/user';
 export class CommentsListComponent implements OnInit {
 
   currentUser: User;
+  deleteIcon = faTrash;
   @Input() comments;
   @Input() article;
   @Output() commentDeleted = new EventEmitter();
 
-  constructor(private http: HttpService) { }
+  constructor(
+    private http: HttpService,
+    private storage: LocalStorageService
+  ) { }
 
   ngOnInit(): void {
-    this.http.getCurrentUser().subscribe( response => this.currentUser = response.user);
+    if(this.storage.getAuthentication()){
+      this.http.getCurrentUser().subscribe( response => this.currentUser = response.user);
+    }
   }
 
   deleteComment(comment: Comment){
     this.http.deleteComment(this.article, comment).subscribe( () => this.commentDeleted.emit() );
   }
 
+  belongsToAuthor(author: Author){
+    if(this.currentUser !== undefined && author.username === this.currentUser.username){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
 }
