231397bbd4d0262a778a0ca29bf6c6a0e4c9a745
Joshua <acciarrijoshua@gmail.com>
2020-09-28 13:17:03 -0300

    URL added to the model and CustomValidator created

--- src/app/todo-form/todo-form.component.ts
+++ src/app/todo-form/todo-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Output, EventEmitter, Input, OnChanges } from '@angular/core';
 import { TodoItem } from '../model/todo-item';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-form',
@@ -10,35 +10,44 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class TodoFormComponent {
   taskForm = new FormGroup({
     taskDescription: new FormControl('', Validators.required),
+    url: new FormControl('', [Validators.required, myUrlValidator])
   });
 
   flagEdit:boolean = false;
   @Output() add = new EventEmitter();
-  @Input() taskEditable = null;
+  @Input() taskEditable:TodoItem = null;
   @Output() save = new EventEmitter();
 
   saveTask() {
     let task = new TodoItem();
     task.description = this.taskForm.value.taskDescription;
+    task.url = this.taskForm.value.url;
     if (this.flagEdit){
       task.id = this.taskEditable.id;
       task.isCompleted = this.taskEditable.isCompleted;
-      this.save.emit(task);
+      this.save.emit(task); 
     }else{
       this.add.emit(task);
     }
     this.flagEdit = false;
-    this.taskForm.setValue({taskDescription: ''});
+    this.taskForm.patchValue({taskDescription: '', url: ''});
   }
 
   ngOnChanges():void{
     if(this.taskEditable){
       this.flagEdit = true;
-      console.log("now is true");
-      
       this.taskForm.patchValue(this.taskEditable);  
     }
   }
 
 }
 
+export function myUrlValidator(control: AbstractControl){
+  const expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+  const regex = new RegExp(expression);
+  if (!control.value.match(regex)){
+    return { urlValid: true}
+  }
+  
+  return null;
+}
