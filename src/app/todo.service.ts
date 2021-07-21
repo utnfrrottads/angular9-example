da14687b9a3747fb6a575b39df8cd31b2b715968
Andres Bertone <andresbertone97@gmail.com>
2021-07-21 16:55:40 -0300

    Ejercicio de servicios con localStorage terminado

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,3 +1,4 @@
+import { TodoItem } from './model/todo-item';
 import { Injectable } from '@angular/core';
 import { LocalStorageService } from './local-storage.service';
 
@@ -6,32 +7,47 @@ import { LocalStorageService } from './local-storage.service';
 })
 export class TodoService {
   
-  list = [];
+  list: TodoItem[] = [];
   lastItemId = 0;
 
   constructor(private storage: LocalStorageService) { }
   
-  add(task) {
+  add(task: TodoItem) {
     const id = this.lastItemId;
     task.id = id;
     this.list.push(task);
-    this.lastItemId += 10;
+    this.lastItemId += 1;
+    this.storage.saveListInLocalStorage( this.list, this.lastItemId );
   }
 
-  remove(id) {
+  remove(id: number) {
     const index = this.list.findIndex((element) => element.id === id);
     this.list.splice(index, 1);
+    if ( this.list.length === 0 ) {
+      this.storage.clearLocalStorage();
+    } else {
+      this.storage.saveListInLocalStorage( this.list, this.lastItemId );
+    }
+  }
+  
+  loadListFromLocalStorage() {
+    let { list, lastItemId } = this.storage.loadListFromLocalStorage();
+    this.list = list;
+    this.lastItemId = lastItemId;
+  }
+
+  updateItemState(task: TodoItem) {
+    task.isCompleted = !task.isCompleted;
+    const index = this.list.findIndex( item => item.id === task.id );
+    this.list[index] = task;
+    this.storage.saveListInLocalStorage( this.list, this.lastItemId );
   }
 
   incompletedSize() {
     return this.list.filter(item => !item.isCompleted).length;
-
   }
+
   completedSize() {
-    return  this.list.filter(item => item.isCompleted).length ;
-  }
-
-  getName() {
-    return 'TodoService 123' + this.storage.getName();
+    return this.list.filter(item => item.isCompleted).length;
   }
 }
